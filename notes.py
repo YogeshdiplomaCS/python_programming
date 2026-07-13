3fac0a8082efe1cd01d79910acce419a2bcd2af1
Yogesh <yogeshagriculture123@gmail.com>
2026-07-13 17:30:40 +0530

    Add interactive story generator in notes.py

---
+++ notes.py
@@ -0,0 +1,5 @@
+name=input("Enter a name :")
+place=input("Enter a place :")
+animal=input("Enter an animal :")
+story=f"{name} went to {place} to see {animal}!"
+print(story + f" and they found a { animal}")
